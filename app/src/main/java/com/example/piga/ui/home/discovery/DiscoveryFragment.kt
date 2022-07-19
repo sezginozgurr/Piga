05c09393055ed9dc1e5a8a71aa7baf9d1b28f160
Özgür Can <sezginzgurr@gmail.com>
2022-07-19 03:11:55 +0300

    Finished DiscoveryFragment

--- app/src/main/java/com/example/piga/ui/home/discovery/DiscoveryFragment.kt
+++ app/src/main/java/com/example/piga/ui/home/discovery/DiscoveryFragment.kt
@@ -1,11 +1,23 @@
 package com.example.piga.ui.home.discovery
 
+import androidx.navigation.fragment.findNavController
+import com.example.piga.R
 import com.example.piga.base.BaseFragment
 import com.example.piga.databinding.FragmentDiscoveryBinding
+import com.example.piga.ui.home.wholikeyou.adapter.WhoLikeYouListAdapter
+import com.example.piga.util.Mock
 
 class DiscoveryFragment :
     BaseFragment<FragmentDiscoveryBinding>(FragmentDiscoveryBinding::inflate) {
 
+    private val adapter by lazy {
+        WhoLikeYouListAdapter {
+            findNavController().navigate(R.id.profileDetailFragment)
+        }
+    }
+
     override fun init() {
+        adapter.items = Mock.getWhoLikeYouList()
+        binding.recyclerView.adapter = adapter
     }
 }
